0e709af56f7aa71092adfd5f81e54903d9a9f137
Vishal Kardam <vishalat1994@gmail.com>
2020-10-19 00:23:45 +0530

    Added copy to clipborad functionality and Alert

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const btn = document.getElementById("flipButton");
 const colorTxt = document.querySelector(".container h2 span");
+let finalHex;
 
 btn.addEventListener('click', function(){
-    let rndmColor = "#"
+    finalHex = randomHex();
+    document.body.style.backgroundColor = finalHex;
+    colorTxt.textContent = finalHex;
+})
 
+function randomHex(){
+    let rndmColor = "#"
     for (let i=0; i<6; i++){
         rndmColor += hex[getRandom()]
     }
-
-    document.body.style.backgroundColor = rndmColor;
-    colorTxt.textContent = rndmColor
-})
+    return rndmColor;
+}
 
 function getRandom() {
     return Math.floor(Math.random() * hex.length);
@@ -28,3 +32,18 @@ burger.addEventListener('click', () => {
     nav.classList.toggle('active')
     main.classList.toggle('active')
 })
+
+function copyHex() {
+
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+
+    dummy.setAttribute("id", "dummy_id");
+    document.getElementById("dummy_id").value = finalHex;
+    console.log(document.getElementById("dummy_id").value)
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+
+    alert("Copied Hex: " + colorTxt.textContent)
+}
